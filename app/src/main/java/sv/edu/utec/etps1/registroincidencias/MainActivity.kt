36a4d7da8a780709ccc0537b6ff332a0f330ceb0
Diego Ramirez <diegoecramirez@gmail.com>
2026-08-28 13:31:16 -0600

    Versión inicial de RegistroIncidencias

---
+++ app/src/main/java/sv/edu/utec/etps1/registroincidencias/MainActivity.kt
@@ -0,0 +1,144 @@
+package sv.edu.utec.etps1.registroincidencias
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import sv.edu.utec.etps1.registroincidencias.ui.theme.RegistroIncidenciasTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RegistroIncidenciasTheme {
+                RegistroIncidenciasApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun RegistroIncidenciasApp() {
+
+    // Se utiliza para reemplazar el texto mostrado por el nuevo
+    // Digitado por el usuario
+    var titulo by remember {
+        mutableStateOf("")
+    }
+
+    var descripcion by remember{
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Registro de Incidencias",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Completa los Datos Básicos para Registrar el Problema."
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        OutlinedTextField(
+            value = titulo,
+            onValueChange = { nuevoTexto ->
+                titulo = nuevoTexto
+            },
+            label = {
+                Text(
+                    text = "Título de la incidencia"
+                )
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = descripcion,
+            onValueChange = { nuevoTexto ->
+                descripcion = nuevoTexto
+            },
+            label = {
+                Text(
+                    text = "Descripción del Problema."
+                )
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(
+            modifier = Modifier.height(24.dp)
+        )
+
+        Button(
+            onClick = { /* Se implementará en una unidad posterior */ }
+        ) {
+            Text(
+                text = "Registrar Incidencia"
+            )
+        }
+
+        Spacer(
+            modifier = Modifier.height(24.dp)
+        )
+
+        Card(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Incidencias Registradas",
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Text(
+                    text = "Ninguna incidencia ha sido registrada."
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = "Prototipo Inicial - Unidad I"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegistroIncidenciasPreview() {
+    RegistroIncidenciasTheme {
+        RegistroIncidenciasApp()
+    }
+}
